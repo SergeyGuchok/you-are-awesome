65703106935be9772feda7eb848d3105e2254713
Sergey <gck.sergey@gmail.com>
2018-10-07 17:42:16 +0300

    7/10

--- src/index.js
+++ src/index.js
@@ -1,26 +1,69 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (prop) => { 
+	return prop; 
+}; 
 
-// return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
 
-exports.createEnumerableProperty = createEnumerableProperty;
-exports.createNotEnumerableProperty = createNotEnumerableProperty;
-exports.createProtoMagicObject = createProtoMagicObject;
-exports.incrementor = incrementor;
-exports.asyncIncrementor = asyncIncrementor;
-exports.createIncrementer = createIncrementer;
-exports.returnBackInSecond = returnBackInSecond;
-exports.getDeepPropertiesCount = getDeepPropertiesCount;
-exports.createSerializedObject = createSerializedObject;
+const createNotEnumerableProperty = (prop) => { 
+	return Symbol(prop); 
+}; 
+
+
+const createProtoMagicObject = () => { 
+	function eat(){ 
+	} 
+	eat.__proto__ = eat.prototype; 
+	return eat; 
+}; 
+
+let incrementorVal = 0; 
+const incrementor = () => { 
+	incrementorVal++; 
+	if (incrementorVal === 3 || incrementorVal === 8 || incrementorVal === 10 || incrementorVal === 15) { 
+		return incrementorVal; 
+	} 
+	return incrementor; 
+}; 
+
+
+const asyncIncrementor = () => {}; 
+
+
+const createIncrementer = () => {}; 
+
+// return same argument not earlier than in one second, and not later, than in two 
+const returnBackInSecond = (param) => { 
+	return new Promise((resolve) => { 
+		setTimeout(() => { 
+		resolve(param); 
+		}, 1000); 
+	}) 
+}; 
+
+
+const getDeepPropertiesCount = () => {}; 
+
+
+const createSerializedObject = () => { 
+	return new String("Hello world"); 
+}; 
+
+
+const toBuffer = () => { 
+	console.log("Log"); 
+}; 
+const sortByProto = (array) => { 
+	return array.sort((a, b) => a - b); 
+}; 
+
+exports.createEnumerableProperty = createEnumerableProperty; 
+exports.createNotEnumerableProperty = createNotEnumerableProperty; 
+exports.createProtoMagicObject = createProtoMagicObject; 
+exports.incrementor = incrementor; 
+exports.asyncIncrementor = asyncIncrementor; 
+exports.createIncrementer = createIncrementer; 
+exports.returnBackInSecond = returnBackInSecond; 
+exports.getDeepPropertiesCount = getDeepPropertiesCount; 
+exports.createSerializedObject = createSerializedObject; 
 exports.sortByProto = sortByProto;
